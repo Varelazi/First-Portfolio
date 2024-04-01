69fbb5da52437207f80392709b4367739263f8ee
Ilene Varela-Zul <ilenevarela03@gmail.com>
2024-04-01 00:31:00 -0700

    Created navbar and added first section of the portfolio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="aboutMe">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function NavBar () {
+    return (
+        <>
+            <div className="navbar">
+                <h2 className="name"> Ilene Varela-Zul </h2>
+                <div className="linkbox">
+                    <h5 className="navlink">About Me</h5>
+                    <h5 className="navlink">Projects</h5>
+                    <h5 className="navlink">Contact Me</h5>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
